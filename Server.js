71661f3d1e215eea5a7f70d49a25b563b79531c6
Abhijith1905 <abhijithkora2005@gmail.com>
2024-04-30 15:28:19 +0530

    V4.0

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+require('dotenv').config();
 
 const dburl = process.env.mongodburl
 mongoose.connect(dburl).then(() => {
